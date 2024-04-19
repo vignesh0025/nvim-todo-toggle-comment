f82f9fbf6b670d7cc945e3acde8d294a7788174e
Vignesh D <vignesh@Vigneshs-MacBook-Air.local>
2024-04-19 09:41:48 +0530

    tests: utils.extract_comment_without_label

--- spec/test_spec.lua
+++ spec/test_spec.lua
@@ -1,19 +1,7 @@
 local utils = require('utils')
 
--- print(utils.extract_comment_without_label("/* TODO: This is todo comment */", "/%*(.+)%*/", "TODO:"))
--- print(extract_comment_without_label("/* FIX: This is fix comment */", "/%*(.+)%*/", "FIX:"))
--- print(extract_comment_without_label("/* This is default comment */", "/%*(.+)%*/", "FIX:"))
--- print(extract_comment_without_label("/*This is default comment with no start end space*/", "/%*(.+)%*/", "FIX:"))
--- print(extract_comment_without_label("/*This is default comment with no end space */", "/%*(.+)%*/", "FIX:"))
--- print(extract_comment_without_label("/* This is default comment with no start space*/", "/%*(.+)%*/", "FIX:"))
--- print(extract_comment_without_label("// FIX: This is fix inline comment", "//(.+)", "FIX:"))
--- print(extract_comment_without_label("//FIX: This is fix inline comment variant 1", "//(.+)", "FIX:"))
--- print(extract_comment_without_label("//FIX:This is fix inline comment variant 2", "//(.+)", "FIX:"))
--- print(extract_comment_without_label("/*TODO: This is todo comment with no start space */", "/%*(.+)%*/", "TODO:"))
--- print(extract_comment_without_label("/*TODO:This is todo comment with no space around space*/", "/%*(.+)%*/", "TODO:"))
---
 --negative cases
--- print(extract_comment_without_label("/* TODO: This is todo comment", "/%*(.+)%*/", "TODO:"))
+-- print(utils.extract_comment_without_label("/* TODO: This is todo comment", "/%*(.+)%*/", "TODO:"))
 
 describe("Busted unit testing framework", function()
   describe("Testing comment extraction from label", function()
@@ -43,16 +31,38 @@ describe("Busted unit testing framework", function()
 		assert.are.same(utils.extract_comment_without_label("/* This is default comment with no start space*/", "/%*(.+)%*/", "FIX:"), "This is default comment with no start space")
 	end)
 
--- print(extract_comment_without_label("// FIX: This is fix inline comment", "//(.+)", "FIX:"))
--- print(extract_comment_without_label("//FIX: This is fix inline comment variant 1", "//(.+)", "FIX:"))
--- print(extract_comment_without_label("//FIX:This is fix inline comment variant 2", "//(.+)", "FIX:"))
--- print(extract_comment_without_label("/*TODO: This is todo comment with no start space */", "/%*(.+)%*/", "TODO:"))
--- print(extract_comment_without_label("/*TODO:This is todo comment with no space around space*/", "/%*(.+)%*/", "TODO:"))
+	it("Default use case", function()
+		assert.are.same(utils.extract_comment_without_label("// FIX: This is fix inline comment", "//(.+)", "FIX:"), "This is fix inline comment")
+	end)
+
+	it("Default use case", function()
+		assert.are.same(utils.extract_comment_without_label("//FIX: This is fix inline comment variant 1", "//(.+)", "FIX:"), "This is fix inline comment variant 1")
+	end)
+
+	it("Default use case", function()
+		assert.are.same(utils.extract_comment_without_label("//FIX:This is fix inline comment variant 2", "//(.+)", "FIX:"), "This is fix inline comment variant 2")
+	end)
+
+	it("Default use case", function()
+		assert.are.same(utils.extract_comment_without_label("/*TODO: This is todo comment with no start space */", "/%*(.+)%*/", "TODO:"), "This is todo comment with no start space")
+	end)
+
+	it("Default use case", function()
+		assert.are.same(utils.extract_comment_without_label("/*TODO:This is todo comment with no space around space*/", "/%*(.+)%*/", "TODO:"), "This is todo comment with no space around space")
+	end)
 
 
     it("Match raw comment extraction", function()
       assert.are.same(utils.extract_comment_without_label("/* FIX: This is fix comment */", "/%*(.+)%*/", "FIX:"), "This is fix comment")
+    end)
 
+	-- Need to wrap error call in function()
+	it("Negative Test", function ()
+		assert.has_error(function() utils.extract_comment_without_label("/* TODO: This is todo comment", "/%*(.+)%*/", "TODO:") end,
+		"ToVignesh: Can't extract comment_text with /%*(.+)%*/ from /* TODO: This is todo comment")
+	end)
+
+    it("Match raw comment extraction", function()
       -- or check by reference!
       -- assert.are_not.equal({ table = "great"}, { table = "great"})
       --
@@ -65,17 +75,19 @@ describe("Busted unit testing framework", function()
       -- assert.has_error(function() error("Wat") end, "Wat")
     end)
 
-	-- it("should provide some shortcuts to common functions", function()
-	-- 	assert.are.unique({{ thing = 1 }, { thing = 2 }, { thing = 3 }})
-	-- end)
-	--
-    -- it("should have mocks and spies for functional tests", function()
-    --   local thing = require("thing_module")
-    --   spy.on(thing, "greet")
-    --   thing.greet("Hi!")
-    --
-    --   assert.spy(thing.greet).was.called()
-    --   assert.spy(thing.greet).was.called_with("Hi!")
-    -- end)
+-- 	print(utils.extract_comment_without_label("/* TODO: This is todo comment", "/%*(.+)%*/", "TODO:"))
+--
+-- 	it("should provide some shortcuts to common functions", function()
+-- 		assert.are.unique({{ thing = 1 }, { thing = 2 }, { thing = 3 }})
+-- 	end)
+--
+-- it("should have mocks and spies for functional tests", function()
+--   local thing = require("thing_module")
+--   spy.on(thing, "greet")
+--   thing.greet("Hi!")
+--
+--   assert.spy(thing.greet).was.called()
+--   assert.spy(thing.greet).was.called_with("Hi!")
+-- end)
   end)
 end)
